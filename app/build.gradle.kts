4f241e652f67542b0ee56b4837b2c17ebc74b511
afterow <99971730+afterow@users.noreply.github.com>
2024-09-20 12:15:31 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release") // 关联签名配置
         }
     }
     compileOptions {
