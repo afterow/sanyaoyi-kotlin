f021a6b9ea684a4ebd81a6b5eddb6bd5183d85cc
afterow <afterow@163.com>
2024-10-10 18:30:09 +0800

    rm splashTheme

---
+++ app/src/main/java/com/afterow/sanyaoyi/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.afterow.sanyaoyi
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        // 延迟3秒（3000毫秒）后跳转到MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // 关闭SplashActivity
+        }, 3000)
+    }
+}
