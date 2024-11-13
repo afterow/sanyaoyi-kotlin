48f56ba84fa985ddb8ff080a09f68798f83e8ad4
afterow <99971730+afterow@users.noreply.github.com>
2024-11-13 13:34:53 +0800

    Deleted some bugs and added some bugs in the future :bug:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "1.5"
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
