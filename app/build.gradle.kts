35c17c6d4e0f4d8fc6b5837d40cc6a8c92477266
afterow <99971730+afterow@users.noreply.github.com>
2025-03-27 09:37:27 +0800

    Release: Version 1.7.2 :bookmark:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "1.7.1rc"
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
