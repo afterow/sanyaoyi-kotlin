331bd26086b3f55910715df5f045290cd53d332b
afterow <99971730+afterow@users.noreply.github.com>
2025-04-06 08:06:50 +0800

    Release: Version 1.7.3 🔖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "1.7.2"
+        versionName = "1.7.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
