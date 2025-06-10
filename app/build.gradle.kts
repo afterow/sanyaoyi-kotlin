c9baa95e5210f1f71171e700bd8f68e3446a529a
afterow <99971730+afterow@users.noreply.github.com>
2025-06-11 02:14:00 +0800

    Release: Version 1.8.2 🔖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "1.8.1"
+        versionName = "1.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
